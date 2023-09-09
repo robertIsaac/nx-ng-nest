42583b655e05f624c0887c57436b28572e18ba8e
robert-isaac_emplc <robert.isaac@fastmarkets.com>
2023-09-10 02:00:09 +0300

    add pm2 ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: 'demo-ng',
+      script: 'dist/ng/server/main.js',
+    },
+    {
+      name: 'demo-nest',
+      script: 'dist/nest/main.js',
+    },
+  ],
+};
