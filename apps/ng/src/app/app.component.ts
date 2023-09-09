9e6ad006dd5ce3e1421e5a5841ba31489bc454dc
robert-isaac_emplc <robert.isaac@fastmarkets.com>
2023-09-10 01:40:27 +0300

    consume nest API in the Angular project

--- apps/ng/src/app/app.component.ts
+++ apps/ng/src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { AppService } from './app.service';
+import { AsyncPipe, JsonPipe } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, AsyncPipe, JsonPipe],
   selector: 'nx-ng-nest-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ng';
+  message$ = this.appService.getMessage();
+
+  constructor(private readonly appService: AppService) {}
 }
